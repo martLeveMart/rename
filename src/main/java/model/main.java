78b15fead004945e0d8a1b87a5a384a3fdcef3c1
martLeveMart <mart.leveque69@gmail.com>
2019-04-13 16:00:51 +0200

    sonar

--- src/main/java/model/main.java
+++ src/main/java/model/main.java
@@ -4,9 +4,7 @@ import model.personnage.mage.Mage;
 
 import java.io.IOException;
 
-public class main {
-    public static void main(String args[]) throws IOException {
-        Mage mage = new Mage();
-        mage.surchargeMagique(10);
+public class Main {
+    public static void Main(String args[]){
     }
 }
